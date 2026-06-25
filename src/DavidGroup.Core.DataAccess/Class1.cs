dbd4728ec708b089a347e015625766053dde8049
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2026-06-25 13:42:39 +0400

    feat: add project structure

---
+++ src/DavidGroup.Core.DataAccess/Class1.cs
@@ -0,0 +1,19 @@
+namespace DavidGroup.Core.DataAccess;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Class1"/> class.
+    /// </summary>
+    public Class1()
+    {
+        int test = new();
+
+        test = 1;
+
+        Console.WriteLine(test);
+    }
+}
