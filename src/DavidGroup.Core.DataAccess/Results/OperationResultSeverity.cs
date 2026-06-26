5182d670117b616f735ffb8d1c2cb8791b9e4650
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2026-06-26 14:46:45 +0400

    feat: add operation results

---
+++ src/DavidGroup.Core.DataAccess/Results/OperationResultSeverity.cs
@@ -0,0 +1,22 @@
+﻿namespace DavidGroup.Core.DataAccess.Results;
+
+/// <summary>
+/// Indicates the severity of the associated result
+/// </summary>
+public enum OperationResultSeverity
+{
+    /// <summary>
+    /// May include note, status or something which contains helpful information.
+    /// </summary>
+    Information,
+
+    /// <summary>
+    /// Shows that it succeeded partially.
+    /// </summary>
+    Warning,
+
+    /// <summary>
+    /// Something went wrong through the operation process.
+    /// </summary>
+    Error
+}
