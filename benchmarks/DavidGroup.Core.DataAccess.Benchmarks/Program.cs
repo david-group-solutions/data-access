0d23082135ac4ad7a76c6182c5380089d9db8913
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2026-07-02 22:41:52 +0400

    feat: add benchmarks project

---
+++ benchmarks/DavidGroup.Core.DataAccess.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+using BenchmarkDotNet.Running;
+
+#if DEBUG
+using BenchmarkDotNet.Configs;
+#endif
+
+#if !DEBUG
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+#else
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args, new DebugInProcessConfig());
+#endif
