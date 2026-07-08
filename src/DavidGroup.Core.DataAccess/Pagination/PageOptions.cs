572bc5b0d02e1fd73d9ce2b1da1fefe827f97cdd
davidkhachatryan123 <davidkhachatryan359@gmail.com>
2026-07-08 13:36:50 +0400

    ref: minor fixes

--- src/DavidGroup.Core.DataAccess/Pagination/PageOptions.cs
+++ src/DavidGroup.Core.DataAccess/Pagination/PageOptions.cs
@@ -30,4 +30,25 @@ public record PageOptions
     /// </summary>
     [JsonConstructor]
     public PageOptions() { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PageOptions"/> class with the specified page number and size.
+    /// </summary>
+    /// <param name="page">The current page number (must be greater than 0).</param>
+    /// <param name="size">The number of items per page (must be greater than 0).</param>
+    /// <exception cref="ArgumentException">
+    /// Thrown if <paramref name="page"/> is less than or equal to 0 or
+    /// if <paramref name="size"/> is less than or equal to 0.
+    /// </exception>
+    public PageOptions(int page, int size)
+    {
+        if (page <= 0)
+            throw new ArgumentException(PaginationErrorMessages.PageNumberShouldBeGreaterThanZero, nameof(page));
+
+        if (size <= 0)
+            throw new ArgumentException(PaginationErrorMessages.PageSizeShouldBeGreaterThanZero, nameof(size));
+
+        Page = page;
+        Size = size;
+    }
 }
